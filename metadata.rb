159909069f483c0be1519748c7aa339409b8edc8
Chris Manson <chris@manson.ie>
2015-02-26 14:22:39 +0000

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'iojs'
+maintainer       'Chris Manson'
+maintainer_email 'chris@bloo.ie'
+license          'MIT'
+description      'Installs/Configures iojs'
+long_description 'Installs/Configures iojs'
+version          '0.1.0'
